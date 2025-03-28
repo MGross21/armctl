5ddfa042180bd11dea754adcc4f569cd65c37e59
Michael Gross <mgstinger521@gmail.com>
2025-03-27 17:51:23 -0700

    Integrate Vention class into main.py for improved robot state management

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from elephant_robotics import ElephantRobotics, Pro600
 from universal_robotics import UniversalRobotics, UR5
 from fanuc import Fanuc
 from AgnosticController import AgnosticController
+from vention import Vention
 
 async def main():
-    # (ElephantRobotics,"192.168.1.159", 5001)
-    # async with AgnosticController(UR5,"192.168.1.111", 30_002) as robot:
-    async with UR5("192.168.1.111", 30_002) as robot:
-        # await robot.home()
-        await robot.move_cartesian([-132,-500,-70 ,0,0,0])
 
+    async with Vention() as vention:
+        await vention.get_robot_state()
+        # await vention.move_joints(100)
+        # await vention.home()
 
 if __name__ == "__main__":
     try:
@@ -20,6 +20,12 @@ if __name__ == "__main__":
         print("Program terminated by user")
     except Exception as e:
         print(e)
-        print("An error occurred")
 
     # print(AgnosticController.supported_manufacturers())
+
+
+    # (ElephantRobotics,"192.168.1.159", 5001)
+    # async with AgnosticController(UR5,"192.168.1.111", 30_002) as robot:
+    # async with UR5("192.168.1.111", 30_002) as robot:
+    #     # await robot.home()
+    #     await robot.move_cartesian([-132,-500,-70 ,0,0,0])
