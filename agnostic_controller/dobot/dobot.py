2eca720d9a27a8fc02ab481a0e1277f539d31e0a
Michael Gross <mgstinger521@gmail.com>
2025-04-01 11:14:30 -0700

    Refactor Dobot class to replace asynchronous methods with synchronous implementations

--- agnostic_controller/dobot/dobot.py
+++ agnostic_controller/dobot/dobot.py
@@ -1,13 +1,13 @@
 from agnostic_controller.templates import SocketController as SCT
 
 class Dobot(SCT):
-    def __init__(self, ip:str, port:int):
+    def __init__(self, ip: str, port: int):
         super().__init__(ip, port)
 
-    async def sleep(self, seconds):
-        await self.send_command(f"sleep({seconds})")
+    def sleep(self, seconds):
+        self.send_command(f"sleep({seconds})")
 
-    async def move_joints(self, joint_positions, *args, **kwargs)->str:
+    def move_joints(self, joint_positions, *args, **kwargs) -> str:
         "MovJ"
 
         if len(joint_positions) != kwargs.get("DOF", 4):
@@ -24,9 +24,9 @@ class Dobot(SCT):
                 raise ValueError(f"Joint {i+1} angle out of range: {low} ~ {high}")
             
         command = "MOVJ({})".format(','.join(map(str, joint_positions)))
-        return await self.send_command(command)
+        return self.send_command(command)
 
-    async def move_cartesian(self, robot_pose, *args, **kwargs)->str:
+    def move_cartesian(self, robot_pose, *args, **kwargs) -> str:
         "MOVEL"
 
         if len(robot_pose) == 3:
@@ -37,24 +37,24 @@ class Dobot(SCT):
             raise ValueError("Robot pose must have 3 ([x, y, z]) or 4 elements: [x, y, z, rz]")
         
         command = "MOVEL({})".format(','.join(map(str, robot_pose)))
-        return await self.send_command(command)
+        return self.send_command(command)
 
-    async def get_joint_positions(self): pass
+    def get_joint_positions(self): pass
 
-    async def get_cartesian_position(self): pass
+    def get_cartesian_position(self): pass
 
-    async def stop_motion(self): pass
+    def stop_motion(self): pass
 
-    async def get_robot_state(self): pass
+    def get_robot_state(self): pass
 
-    async def move_arc(self, command):
+    def move_arc(self, command):
         """The trajectory of ARC mode is an arc, which is determined by three points (the current point, any point and the end point on the arc)"""
         if len(command) != 3:
             raise ValueError("Invalid ARC command. Must have 3 points")
         
         self.send_command(f"ARC({','.join(map(str, command))})")
 
-    async def move_jump(self, command):
+    def move_jump(self, command):
         """If the movement of two points is required to lift upwards by amount of height, such as sucking up, grabbing, you can choose JUMP"""
         if len(command) != 2:
             raise ValueError("Invalid JUMP command. Must have 2 points")
