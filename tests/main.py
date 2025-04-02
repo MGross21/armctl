aac78b13ffa49f8e245aa06710d69e432289ba44
Michael Gross <mgstinger521@gmail.com>
2025-04-02 15:43:14 -0700

    Implement parallel movement for Vention and UR5 using threading

--- tests/main.py
+++ tests/main.py
@@ -1,6 +1,6 @@
-import time
 import sys
 from pathlib import Path
+import threading
 
 # Add the parent directory of the current script to the system path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
@@ -13,13 +13,27 @@ from agnostic_controller import (
     Vention
 )
 
-def vention_ur5():
+pos1_low = [0.6116023063659668, -0.8117355865291138, 0.8821848074542444, -1.6617809734740199, -1.581780258809225, 0.3699551522731781]
+pos2_high = [0.8180813789367676, -1.0786418181708832, 1.1617329756366175, -1.6617847881712855, -1.581768814717428, 0.3699668049812317]
 
-    with Vention() as vention:
-        vention.home()
-        # time.sleep()
-        # vention.reset()
-        # vention.move_joints(200, speed=2_500, acceleration=500, move_type='abs') # 2500/500 is decent speed
+def move_vention(vention:Vention,pos):
+    vention.move_joints(pos, speed=2_500, acceleration=500, move_type='abs')
+
+def move_ur5(ur5:UR5,pos):
+    ur5.move_joints(pos, time=3)
+
+def vention_ur5():
+    with Vention("192.168.7.2") as vention, UR5("192.168.1.111") as ur5:
+        vention_thread = threading.Thread(target=move_vention, args=(vention, 500))
+        ur5_thread = threading.Thread(target=move_ur5, args=(ur5, pos1_low))
+
+        # Start both movements in parallel
+        vention_thread.start()
+        ur5_thread.start()
+
+        # Wait for both to finish
+        vention_thread.join()
+        ur5_thread.join()
         # # vention.reset()
 
 
@@ -34,9 +48,11 @@ def ur():
     with UR5("192.168.1.111") as ur5:
         # [36, -45, 47, -93, -91, 22] -? [....,0]
         angle = ur5.get_joint_positions()
-        print(tuple(a * 180 / 3.14 for a in angle))
-        cart = ur5.get_cartesian_position()  # Uncomment to get cartesian position
-        print(cart)
+        pos1_low = [0.6116023063659668, -0.8117355865291138, 0.8821848074542444, -1.6617809734740199, -1.581780258809225, 0.3699551522731781]
+        pos2_high = [0.8180813789367676, -1.0786418181708832, 1.1617329756366175, -1.6617847881712855, -1.581768814717428, 0.3699668049812317]
+        ur5.move_joints(pos1_low, time=3)
+        # cart = ur5.get_cartesian_position()  # Uncomment to get cartesian position
+        # print(cart)
 
 
 def mycobot():
@@ -47,4 +63,4 @@ def mycobot():
         
 
 if __name__ == "__main__":
-    ur()
+    vention_ur5()
