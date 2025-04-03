82cb69db852cdff916b2a03dae2d62c191febda3
Michael Gross <mgstinger521@gmail.com>
2025-04-02 17:10:33 -0700

    FIX: HOME_POSITION in UR5 class to use radians for joint angles

--- agnostic_controller/universal_robotics/ur5.py
+++ agnostic_controller/universal_robotics/ur5.py
@@ -1,9 +1,10 @@
 from .universal_robotics import UniversalRobotics as UR
+import math
 
 class UR5(UR):
         def __init__(self, ip:str):
             super().__init__(ip)
-            self.HOME_POSITION = [0, -90, 90, -90, -90, 0]
+            self.HOME_POSITION = [math.pi/2, -math.pi/2, math.pi/2, -math.pi/2, -math.pi/2, 0] # VERIFIED VENTION TABLE UR5: 4/2/2025
 
         def home(self):
             self.move_joints(self.HOME_POSITION, speed=0.1)
