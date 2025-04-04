293f39f92bb336cd966b2a2e1e4458a1fac2be31
Michael Gross <mgstinger521@gmail.com>
2025-04-04 15:03:35 -0700

    Remove unused IKSolver and PathPlanner classes; update project version and dependencies in pyproject.toml; refactor main.py for improved threading and robot control

--- tests/main.py
+++ tests/main.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 import threading
+import time
 
 # Add the parent directory of the current script to the system path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
@@ -30,20 +31,39 @@ def vention_ur5():
     with Vention("192.168.7.2") as vention, UR5("192.168.1.111") as ur5:
 
         # HOMING
-        ur_home = threading.Thread(target=vention.home)
-        vention_home = threading.Thread(target=ur5.home)
-        ur_home.start()
-        vention_home.start()
-        ur_home.join()
-        vention_home.join()
+        lock = threading.Lock()
+        def ur5_home():
+            with lock:
+                ur5.home()
+
+        # def vention_home_func():
+        #     with lock:
+        #         vention.home()
+
+        # threads = [
+        threading.Thread(target=ur5_home)
+        #     threading.Thread(target=vention_home_func)
+        # ]
+
+        # for thread in threads:
+        #     thread.start()
+
+        # for thread in threads:
+        #     thread.join()
+
+        # ur5.set_gripper_position(200)
+        # ur5.set_gripper_position(name="rg6", position=10)
+        # ur5.home()
+
+        # Ensure both robots finish their tasks before proceeding
 
         # MOVING
-        vention_thread = threading.Thread(target=move_vention, args=(vention, 800))
-        ur5_thread = threading.Thread(target=move_ur5, args=(ur5, close_to_table))
-        vention_thread.start()
-        ur5_thread.start()
-        vention_thread.join()
-        ur5_thread.join()
+        # vention_thread = threading.Thread(target=move_vention, args=(vention, 800))
+        # ur5_thread = threading.Thread(target=move_ur5, args=(ur5, close_to_table))
+        # vention_thread.start()
+        # ur5_thread.start()
+        # vention_thread.join()
+        # ur5_thread.join()
         # # vention.reset()
 
 
@@ -54,15 +74,19 @@ def vention_ur5():
         # # # await ur5.home()
 
 
-def ur():
+def ur5():
     with UR5("192.168.1.111") as ur5:
         # [36, -45, 47, -93, -91, 22] -? [....,0]
-        angle = ur5.get_joint_positions()
+        angle = ur5.home()
+        time.sleep(10)
+        
         # pos1_low = [0.6116023063659668, -0.8117355865291138, 0.8821848074542444, -1.6617809734740199, -1.581780258809225, 0.3699551522731781]
         # pos2_high = [0.8180813789367676, -1.0786418181708832, 1.1617329756366175, -1.6617847881712855, -1.581768814717428, 0.3699668049812317]
         # ur5.move_joints(pos1_low, time=3)
         # cart = ur5.get_cartesian_position()  # Uncomment to get cartesian position
         # print(cart)
+
+
 
 
 def mycobot():
@@ -73,4 +97,4 @@ def mycobot():
         
 
 if __name__ == "__main__":
-    vention_ur5()
+    ur5()
