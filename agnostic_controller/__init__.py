20cd9d3ff9b2313c4efa4236960a98079d66f385
Michael Gross <mgstinger521@gmail.com>
2025-04-04 22:15:38 -0700

    Add OnRobot manipulator import to the agnostic_controller module

--- agnostic_controller/__init__.py
+++ agnostic_controller/__init__.py
@@ -21,3 +21,4 @@ from .elephant_robotics import ElephantRobotics, Pro600
 from .universal_robotics import UniversalRobotics, UR5
 from .fanuc import Fanuc
 from .vention import Vention
+from .manipulators import OnRobot
