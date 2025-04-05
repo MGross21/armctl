734780ffc2b4e20b4fde8b86258fd506f0ff6b7b
Michael Gross <mgstinger521@gmail.com>
2025-04-05 15:32:59 -0700

    Refactor main.py: reorganize imports, simplify gripper function, and update entry point to call gripper

--- tests/main.py
+++ tests/main.py
@@ -11,7 +11,8 @@ from agnostic_controller import (
     ElephantRobotics, Pro600, 
     UniversalRobotics, UR5, 
     Fanuc, 
-    Vention
+    Vention,
+    OnRobot
 )
 
 pos1_low = [0.6116023063659668, -0.8117355865291138, 0.8821848074542444, -1.6617809734740199, -1.581780258809225, 0.3699551522731781]
@@ -27,58 +28,20 @@ def move_ur5(ur5:UR5,pos):
     ur5.move_joints(pos, t=3)
     # ur5.home()
 
+def gripper():
+    with OnRobot() as gripper:
+        gripper.open()
+
+
 def vention_ur5():
     with Vention("192.168.7.2") as vention, UR5("192.168.1.111") as ur5:
-
-        # HOMING
-        lock = threading.Lock()
-        def ur5_home():
-            with lock:
-                ur5.home()
-
-        # def vention_home_func():
-        #     with lock:
-        #         vention.home()
-
-        # threads = [
-        threading.Thread(target=ur5_home)
-        #     threading.Thread(target=vention_home_func)
-        # ]
-
-        # for thread in threads:
-        #     thread.start()
-
-        # for thread in threads:
-        #     thread.join()
-
-        # ur5.set_gripper_position(200)
-        # ur5.set_gripper_position(name="rg6", position=10)
-        # ur5.home()
-
-        # Ensure both robots finish their tasks before proceeding
-
-        # MOVING
-        # vention_thread = threading.Thread(target=move_vention, args=(vention, 800))
-        # ur5_thread = threading.Thread(target=move_ur5, args=(ur5, close_to_table))
-        # vention_thread.start()
-        # ur5_thread.start()
-        # vention_thread.join()
-        # ur5_thread.join()
-        # # vention.reset()
-
-
-        
-        # # ur5.get_robot_state()
-        
-        # # await vention.home()
-        # # # await ur5.home()
-
+        pass
 
 def ur5():
     with UR5("192.168.1.111") as ur5:
         # [36, -45, 47, -93, -91, 22] -? [....,0]
         angle = ur5.home()
-        time.sleep(10)
+        # time.sleep(10)
         
         # pos1_low = [0.6116023063659668, -0.8117355865291138, 0.8821848074542444, -1.6617809734740199, -1.581780258809225, 0.3699551522731781]
         # pos2_high = [0.8180813789367676, -1.0786418181708832, 1.1617329756366175, -1.6617847881712855, -1.581768814717428, 0.3699668049812317]
@@ -97,4 +60,4 @@ def mycobot():
         
 
 if __name__ == "__main__":
-    ur5()
+    gripper()
