218cdf30edddec09ab47e72b5a91bb9b3bcddbbc
MGross21 <mgstinger521@gmail.com>
2025-08-03 16:12:43 -0700

    Update README and refactor Dobot class with Socket Version: clarify documentation and improve class structure

--- armctl/dobot/README.md
+++ armctl/dobot/README.md
@@ -3,8 +3,9 @@
 ## Documentation
 
 > [!Note]
-> Comprehensive documentation for the Dobot is limited, reflecting the incomplete state of controller support in this project. English resources may be sparse or unofficial.
+> Comprehensive documentation for the Dobot is limited. English resources may be sparse or unofficial.
 
 - Serial Communication Guide ([Community Post](https://www.littlechip.co.nz/blog/communicating-with-the-dobot-magician-using-raw-protocol))
 
 - Magician Pro ([Handbook](https://wiki.idiot.io/_media/dobot-magician-user-guide-v1.7.0.pdf))
+- M1 Pro [TCP/IP Guide](https://download.dobot.cc/2024/04/TCP_IP%20Remote%20Control%20Interface%20Guide%20%284axis%29_20240419_en.pdf)
--- armctl/dobot/dobot.py
+++ armctl/dobot/dobot.py
@@ -1,7 +1,11 @@
-from armctl.templates import SerialController as SCT
+from armctl.templates import SerialController as Serial
+from armctl.templates import SocketController as Socket
+from armctl.templates import Commands
+
+from time import sleep as _sleep
 
 
-class Dobot(SCT):
+class DobotSerial(Serial, Commands):
     def __init__(self, ip: str, port: int):
         super().__init__(ip, port)
         self.JOINT_RANGES = [
@@ -11,9 +15,6 @@ class Dobot(SCT):
             (-145.00, 145.00),
         ]
         self.DOF = len(self.JOINT_RANGES)
-        raise NotImplementedError(
-            f"{self.__class__.__name__.upper()} is not yet supported."
-        )
 
     def sleep(self, seconds):
         self.send_command(f"sleep({seconds})")
@@ -59,3 +60,143 @@ class Dobot(SCT):
 
     def get_robot_state(self):
         pass
+
+
+
+class Codes:
+    COMMAND = {
+        0: "No error: Delivered successfully",
+        5: "Robot in Motion (Immediate Execution)",
+        7: "Robot in Motion (Queued)",
+        -1: "Failed to receive or execute",
+        -10000: "Command does not exist",
+        -20000: "Incorrect number of command parameters",
+        -30001: "First parameter type incorrect",
+        -30002: "Second parameter type incorrect",
+        -30003: "Third parameter type incorrect",
+        -30004: "Fourth parameter type incorrect",
+        -40001: "First parameter out of range",
+        -40002: "Second parameter out of range",
+        -40003: "Third parameter out of range",
+        -40004: "Fourth parameter out of range",
+    }
+
+    STATE = {
+        1: "ROBOT_MODE_INIT - Initialized status",
+        2: "ROBOT_MODE_BRAKE_OPEN - Brake switched on",
+        3: "ROBOT_MODE_POWER_STATUS - Power-off status",
+        4: "ROBOT_MODE_DISABLED - Disabled (no brake switched on)",
+        5: "ROBOT_MODE_ENABLE - Enabled and idle (no project running, no alarm)",
+        6: "ROBOT_MODE_BACKDRIVE - Drag mode",
+        7: "ROBOT_MODE_RUNNING - Running status (including trajectory playback/fitting, executing motion commands, running project)",
+        8: "ROBOT_MODE_RECORDING - Trajectory recording mode",
+        9: "ROBOT_MODE_ERROR - There are uncleared alarms. This status has the highest priority. It returns 9 when there is an alarm, regardless of the status of the robot arm.",
+        10: "ROBOT_MODE_PAUSE - Pause status",
+        11: "ROBOT_MODE_JOG - Jogging status",
+    }
+
+    @classmethod
+    def get_cmd_message(cls, code: int) -> str:
+        return cls.COMMAND.get(code, f"Unknown error code: {code}")
+
+    @classmethod
+    def get_state_message(cls, code: int) -> str:
+        return cls.STATE.get(code, f"Unknown state code: {code}")
+
+class DobotSocket(Socket, Commands):
+    def __init__(self, ip: str, port: int | tuple[int, int] = (30_003, 30_004)):
+        super().__init__(ip, port)
+
+    def _send(self, s: str):
+        """
+        Message Response Format:
+        "ErrorID,{value,...,valueN},Message name(Param1,Param2,Param3……ParamN);"
+
+        - If ErrorID is 0, the command is received successfully. 
+        - {value,...,valueN} refers to the return value. {} means no return value.
+        - Message name(Param1,Param2,Param3……ParamN) refers to the content delivered.
+        """
+        c,r,m = self.send_command(f"{s}\n").strip().split(sep=",")
+        code = int(c)
+        response = r.strip("{}")
+        message = m.strip(";")
+        return  code, response, message
+
+    def _send_and_check(self, cmd: str, motion: bool = False) -> list[float] | None:
+        """Send a command to the robot and check for errors in the response."""
+
+        code, response, _ = self._send(cmd) # Queue Command
+
+        if 0 == code and response == "":
+            # Return the values inside the curly braces as a list, or None if empty
+            result = [float(x) for x in response.split(",")] if response else None
+
+            if motion:
+                self._send("Sync()")
+                code2, _, _ = self._send("RobotMode()")  # Get the robot
+                # TODO: check order of status codes
+                # if code2 in (5, 7):
+                #     pass
+                # else:
+                #     raise RuntimeError(
+                #         f"Error {code2}: {Codes.get_cmd_message(code2)}"
+                #     )
+            return result
+        else:
+            raise RuntimeError(
+                f"Error {code}: {Codes.get_cmd_message(code)}"
+            )
+        
+    def connect(self):
+        super().connect()
+        # 0, 1, or all 4 parameters can be set to enable the robot.
+        enable = {
+            "load": 0.0,  # Load in kg
+            "centerX": 0.0,  # Center X in mm (-500 to 500)
+            "centerY": 0.0,  # Center Y in mm (-500 to 500)
+            "centerZ": 0.0,  # Center Z in mm (-500 to 500)
+        }
+        self._send_and_check("EnableRobot({},{},{},{})".format(
+            enable["load"],
+            enable["centerX"],
+            enable["centerY"],
+            enable["centerZ"]
+        ))
+
+        # Clear the alarms of the robot
+        self._send_and_check("ClearError()")
+        # Required to restart motion Queue after clearing alarms
+        self._send_and_check("Continue()")
+        # Stop the robot and clear the planned command queue
+        self._send_and_check("ResetRobot()")
+
+    def disconnect(self):
+        # self._send_and_check("StopScript()")
+
+        super().disconnect()
+
+    def get_robot_state(self) -> str:
+        """Get the current state of the robot."""
+        code, _, _ = self._send("RobotMode()")
+        return Codes.get_state_message(code)
+    
+    def get_joint_positions(self):
+        """Get the joint coordinates of current posture"""
+        return self._send_and_check("GetAngle()")
+    
+    def get_cartesian_position(self):
+        """Get the Cartesian coordinates of current posture"""
+        return self._send_and_check("GetPose()")
+    
+    def move_cartesian(self, pose):
+        self._send_and_check(
+            "MoveJ({},{},{},{})".format(*pose), motion=True
+        )
+
+    def move_joints(self, pos):
+        self._send_and_check(
+            "JointMoveJ({})".format(",".join(map(str, pos))), motion=True
+        )
+
+    def sleep(self, seconds):
+        _sleep(seconds)
